db6492f0b487bf639df4f3fb999f08b817e59bab
Donjetë <43152238+DonjeteHasani@users.noreply.github.com>
2023-05-02 21:53:16 +0200

    update

--- api/login.php
+++ api/login.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login Page</title>
-    <link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1/css/bootstrap.min.css">
-      <script type="text/javascript" src="../bootstrap-4.4.1/js/juqery_latest.js"></script>
-      <script type="text/javascript" src="../bootstrap-4.4.1/js/bootstrap.min.js"></script>
-    <style type="text/css">
-
-    body{
-        background-color:#242943 ;
-    }
-        h3{
-            font-size: 50px;
-            font-weight: bold;
-            font-family: 'Potta One', cursive;
-            color: #1fcfb1;
-        }
-        h3{
-            position: absolute;
+	<title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap-4.4.1/css/bootstrap.min.css">
+  	<script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
+  	<script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
+	<style type="text/css">
+	
+	body{
+		background-color:#242943 ;
+	}
+		h3{
+			font-size: 50px;
+			font-weight: bold;
+			font-family: 'Potta One', cursive;
+			color: #1fcfb1;
+		}
+		h3{
+			position: absolute;
   left: 335px;
   top: 45px;
-        }
-        #al,#ul{
-             padding-top: 60px; /
-            font-size:20px;
-            background:#132347;
-            color: aliceblue;
-            height: 50px;
-            width: 250px;
-            font-family: 'Potta One', cursive;
-            border-color: aliceblue;
-        }
-        #al,#ul {
+		}
+		#al,#ul{
+			/* padding-top: 60px; */
+			font-size:20px;
+			background:#132347;
+			color: aliceblue;
+			height: 50px;
+    		width: 250px;
+			font-family: 'Potta One', cursive;
+			border-color: aliceblue;
+		}
+		#al,#ul {
   box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
 }
 #al span,#ul span {
@@ -41,11 +41,11 @@
   transition: 0.5s;
 }
 
-        #al:hover,#ul:hover{
-            background: aliceblue;
-            color: #132347;
-        }
-        #al span:after,#ul span:after {
+		#al:hover,#ul:hover{
+			background: aliceblue;
+			color: #132347;
+		}
+		#al span:after,#ul span:after {
   content: '\00bb';
   position: absolute;
   opacity: 0;
@@ -83,107 +83,108 @@ position:absolute;
 top:265px;
 }
 @keyframes geethu {
-        0% {
-            / right: -4em; */
-top: -4em;
-            opacity: 0;
-        }
-        100% {
+		0% {
+			/* right: -4em; */
+			top: -4em;
+			opacity: 0;
+		}
+		100% {
 
-            top: 220px;
-            opacity: 1;
-            right: 505px;
-        }
-    }
-    @keyframes geeth {
-        0% {
-            right: -4em; /
-            top: 360px;
-            opacity: 0;
-        }
+			top: 220px;
+			opacity: 1;
+			right: 505px;
+		}
+	}
+	@keyframes geeth {
+		0% {
+			/* right: -4em; */
+			top: 360px;
+			opacity: 0;
+		}
 
-        100% {
-            top: 360px;
-            opacity: 1;
-            right: 505px;
-        }
-    }
+		100% {
+			top: 360px;
+			opacity: 1;
+			right: 505px;
+		}
+	}
 
-    @keyframes geet {
-        0% {
-            / right: -4em; */
-            top: 220px;
-            opacity: 0;
-        }
+	@keyframes geet {
+		0% {
+			/* right: -4em; */
+			top: 220px;
+			opacity: 0;
+		}
 
-        100% {
-            top: 220px;
-            opacity: 1;
-            right: 505px;
-        }
-    }
+		100% {
+			top: 220px;
+			opacity: 1;
+			right: 505px;
+		}
+	}
 
-    #nav
+	#nav
 {
-    background: #242943;
-    height:70px;
-    width:100%;
-    line-height: 60px;
-    animation: geethu 3.5s ease-in-out forwards;
-    border:aliceblue;
+	background: #242943;
+	height:70px;
+	width:100%;
+	line-height: 60px;
+	animation: geethu 3.5s ease-in-out forwards;
+	border:aliceblue;
 }
 #nav h1{
-    float: left;
+	float: left;
 }
 #nav h1 a{
-    position: absolute;
-    text-decoration: none;
-    color: white;
-    font-family: 'Potta One', cursive;
-    margin-left: 25px;
-    top:10px;
+	position: absolute;
+	text-decoration: none;
+	color: white;
+	font-family: 'Potta One', cursive;
+	margin-left: 25px;
+	top:10px;
 }
 #nav ul{
-    float: right;
+	float: right;
 }
 #nav ul li{
-    text-decoration: none;
-    list-style-type: none;
-    display: inline-block;
+	text-decoration: none;
+	list-style-type: none;
+	display: inline-block;
 }
 #nav ul li a{
-    text-decoration: none;
-    color: white;
-    font-weight: bold;
-    font-family: 'Potta One', cursive;
-    padding: 25px;
+	text-decoration: none;
+	color: white;
+	font-weight: bold;
+	font-family: 'Potta One', cursive;
+	padding: 25px;
 }
 #nav ul li :hover{
-    background: aliceblue;
-    color: #132347;
-    transition: all ease-in-out 0.40s;
+	background: aliceblue;
+	color: #132347;
+	transition: all ease-in-out 0.40s;
 }
 #ll{
-    position: absolute;
-    top: 265px;
-    left: 980px;
+	position: absolute;
+	top: 265px;
+	left: 980px;
 }
 
 </style>
 </head>
 <body >
 <div id="nav">
-
-        <img id="ml" src="../images/vol.png"  height="70" width="70" align="left"> 
-        <h1><a href="login.php">Volunteer System<BR CLEAR="left" /></a></h1>
+		
+		<img id="ml" src="vol.png"  height="70" width="70" align="left"> 
+		<h1><a href="login.php">Volunteer System<BR CLEAR="left" /></a></h1>
     <ul>
         <li><a href="index.html">HOME</a></li>
         <li><a href="contact.html">CONTACT US</a></li>
-
+        
           <li><a href="login.php">LOGIN</a></li>
-          <hr style="height:2px;border-width:0;color:gray;background-color:white">
+		  <hr style="height:2px;border-width:0;color:gray;background-color:white">
+    </li>
     </ul>
-
+	
 </div>
     <center><br><br><br></br>
 <form action="" method="POST" id="lf">
