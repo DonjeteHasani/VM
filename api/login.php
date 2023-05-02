7c37bba29b030a4f70b9f5644e38a8ac97efb2f2
Donjetë <43152238+DonjeteHasani@users.noreply.github.com>
2023-05-02 18:57:55 +0200

    ..

--- api/login.php
+++ api/login.php
@@ -2,9 +2,9 @@
 <html>
 <head>
     <title>Login Page</title>
-    <link rel="stylesheet" type="text/css" href="bootstrap-4.4.1/css/bootstrap.min.css">
-      <script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
-      <script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1/css/bootstrap.min.css">
+      <script type="text/javascript" src="../bootstrap-4.4.1/js/juqery_latest.js"></script>
+      <script type="text/javascript" src="../bootstrap-4.4.1/js/bootstrap.min.js"></script>
     <style type="text/css">
 
     body{
