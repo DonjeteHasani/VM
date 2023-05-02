860e432c5f8b55d43f2ca0a76e4fbffb9f056dbc
Donjetë <43152238+DonjeteHasani@users.noreply.github.com>
2023-05-02 22:57:24 +0200

    update

--- api/add_student.php
+++ api/add_student.php
@@ -1,7 +1,7 @@
 <?php
 	$connection = mysqli_connect("db4free.net:3306","rilinda","cfa4b2b5");
 	$db = mysqli_select_db($connection,"volunteerapp");
-	$query = "INSERT INTO students VALUES('',$_POST[roll_no],'$_POST[name]','$_POST[father_name]',$_POST[class],'$_POST[mobile]','$_POST[email]','$_POST[password]','$_POST[remark]')";
+	$query = "INSERT INTO students VALUES('$_POST['s_no']',$_POST[roll_no],'$_POST[name]','$_POST[father_name]',$_POST[class],'$_POST[mobile]','$_POST[email]','$_POST[password]','$_POST[remark]')";
 	$query_run = mysqli_query($connection,$query);
 ?>
 <script type="text/javascript">
